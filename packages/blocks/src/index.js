84d65727d3c7585af3b2e41b97bf391299a1842b
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2022-05-27 11:48:36 +0200

    Blocks: Remove no longer used Block Content Context

--- packages/blocks/src/index.js
+++ packages/blocks/src/index.js
@@ -10,4 +10,16 @@
 
 export { store } from './store';
 export * from './api';
-export { withBlockContentContext } from './block-content-provider';
+
+/**
+ * A Higher Order Component used to inject BlockContent using context to the
+ * wrapped component.
+ *
+ * @deprecated
+ *
+ * @param {WPComponent} OriginalComponent The component to enhance.
+ * @return {WPComponent} The same component.
+ */
+export function withBlockContentContext( OriginalComponent ) {
+	return OriginalComponent;
+}
