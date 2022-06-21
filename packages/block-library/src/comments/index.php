e0cbd27d985243c6de0fd234975f93876abd310f
David Arenas <david.arenas@automattic.com>
2022-06-21 13:13:36 +0200

    Remove post-comment block

--- packages/block-library/src/comments/index.php
+++ packages/block-library/src/comments/index.php
@@ -16,8 +16,8 @@
 function render_block_core_comments_query_loop( $attributes, $content, $block ) {
 	global $post;
 
-	// Do we have child blocks?
-	if ( count( $block->inner_blocks ) > 0 ) {
+	$is_legacy = 'core/post-comments' === $block->name || isset( $attributes['legacy'] );
+	if ( ! $is_legacy ) {
 		$inner_blocks_html = '';
 		foreach ( $block->inner_blocks as $inner_block ) {
 			$inner_blocks_html .= $inner_block->render();
@@ -95,3 +95,92 @@ function comments_query_loop_block_form_defaults( $fields ) {
 	return $fields;
 }
 add_filter( 'comment_form_defaults', 'comments_query_loop_block_form_defaults' );
+
+/**
+ * Renders the legacy `core/post-comments` block on the server.
+ * It triggers a developer warning and then calls the renamed
+ * block's `render_callback` function output.
+ *
+ * This can be removed when WordPress X.X is released.
+ *
+ * @param array    $attributes Block attributes.
+ * @param string   $content    Block default content.
+ * @param WP_Block $block      Block instance.
+ *
+ * @return string Returns the output of the block.
+ */
+function render_legacy_post_comments_block( $attributes, $content, $block ) {
+	trigger_error(
+		/* translators: %1$s: Block type */
+		sprintf( __( 'Block %1$s has been renamed to Comments Query Loop. %1$s will be supported until WordPress version X.X.', 'gutenberg' ), $block->name ),
+		headers_sent() || WP_DEBUG ? E_USER_WARNING : E_USER_NOTICE
+	);
+	return render_block_core_comments_query_loop( $attributes, $content, $block );
+}
+
+/**
+ * Ensures backwards compatibility for any users running the Gutenberg plugin
+ * who have used Post Comments before it was merged into Comments Query Loop.
+ *
+ * The same approach was followed when core/query-loop was renamed to
+ * core/post-template.
+ *
+ * This can be removed when WordPress X.X is released.
+ *
+ * @see https://github.com/WordPress/gutenberg/pull/41807
+ * @see https://github.com/WordPress/gutenberg/pull/32514
+ */
+function gutenberg_register_legacy_post_comments_block() {
+	$registry = WP_Block_Type_Registry::get_instance();
+	if ( $registry->is_registered( 'core/post-comments' ) ) {
+		unregister_block_type( 'core/post-comments' );
+	}
+	register_block_type(
+		'core/post-comments',
+		array(
+			'category'          => 'theme',
+			'attributes'        => array(
+				'textAlign' => array(
+					'type' => 'string',
+				),
+			),
+			'uses_context'      => array(
+				'postId',
+				'postType',
+			),
+			'supports'          => array(
+				'html'       => false,
+				'align'      => array( 'wide', 'full' ),
+				'typography' => array(
+					'fontSize'                      => true,
+					'lineHeight'                    => true,
+					'__experimentalFontStyle'       => true,
+					'__experimentalFontWeight'      => true,
+					'__experimentalLetterSpacing'   => true,
+					'__experimentalTextTransform'   => true,
+					'__experimentalDefaultControls' => array(
+						'fontSize' => true,
+					),
+				),
+				'color' => array(
+					'gradients'                     => true,
+					'link'                          => true,
+					'__experimentalDefaultControls' => array(
+						'background' => true,
+						'text'       => true
+					)
+				),
+				'inserter' => false
+			),
+			'style'             => array(
+				'wp-block-post-comments',
+				'wp-block-buttons',
+				'wp-block-button'
+			),
+			'editorStyle'       => 'wp-block-post-comments-editor',
+			'render_callback'   => 'render_legacy_post_comments_block',
+			'skip_inner_blocks' => true,
+		)
+	);
+}
+add_action( 'init', 'gutenberg_register_legacy_post_comments_block', );
