ff79f775e5a287c93b4730b00e5cfe00fb513914
ntsekouras <ntsekouras@outlook.com>
2022-07-06 18:09:58 +0300

    [Full Site Editing]: Update descrptions for creating taxonomy templates

--- packages/edit-site/src/components/add-new-template/utils.js
+++ packages/edit-site/src/components/add-new-template/utils.js
@@ -106,6 +106,13 @@ export const entitiesConfig = {
 	taxonomy: {
 		...taxonomyBaseConfig,
 		templatePrefix: 'taxonomy-',
+		getDescription: ( labels, slug ) =>
+			sprintf(
+				// translators: %1s: Name of the taxonomy e.g: "Product Categories"; %2s: Slug of the taxonomy e.g: "product-categories".
+				__( 'Displays a single taxonomy: %1$s (%2$s).' ),
+				labels.singular_name,
+				slug
+			),
 	},
 	category: { ...taxonomyBaseConfig },
 	tag: { ...taxonomyBaseConfig, templateSlug: 'tag' },
@@ -317,7 +324,10 @@ export const useExtraTemplates = (
 				: {
 						slug: generalTemplateSlug,
 						title: entityConfig.getTitle( labels ),
-						description: entityConfig.getDescription( labels ),
+						description: entityConfig.getDescription(
+							labels,
+							slug
+						),
 						icon: entityConfig.getIcon?.( icon ),
 				  };
 			const hasEntities = entitiesInfo?.[ slug ]?.hasEntities;
